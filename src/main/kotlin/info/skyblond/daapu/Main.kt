ac2daf91676da93988e97b226136daa428bd069e
Rui Hu <hurui200320@skyblond.info>
2026-08-04 00:29:16 +0800

    handle streaming and figure out the correct parameter to trigger reasoning for novita
    The reasoning content is put into field reasoning_details

--- src/main/kotlin/info/skyblond/daapu/Main.kt
+++ src/main/kotlin/info/skyblond/daapu/Main.kt
@@ -2,8 +2,11 @@ package info.skyblond.daapu
 
 import ai.koog.agents.chatMemory.feature.ChatMemory
 import ai.koog.agents.core.agent.AIAgent
+import ai.koog.agents.core.agent.config.AIAgentConfig
 import ai.koog.agents.core.dsl.builder.node
 import ai.koog.agents.core.dsl.builder.strategy
+import ai.koog.agents.core.tools.ToolRegistry
+import ai.koog.prompt.dsl.prompt
 import ai.koog.prompt.executor.clients.openai.OpenAIClientSettings
 import ai.koog.prompt.executor.clients.openai.OpenAILLMClient
 import ai.koog.prompt.executor.llms.MultiLLMPromptExecutor
@@ -11,14 +14,17 @@ import ai.koog.prompt.llm.LLMCapability
 import ai.koog.prompt.llm.LLMProvider
 import ai.koog.prompt.llm.LLModel
 import ai.koog.prompt.message.Message
-import ai.koog.prompt.message.MessagePart
-import ai.koog.prompt.message.ResponseMetaInfo
+import ai.koog.prompt.params.LLMParams
 import ai.koog.prompt.streaming.StreamFrame
+import ai.koog.prompt.streaming.requireEndFrame
+import ai.koog.prompt.streaming.toMessageResponse
 import info.skyblond.daapu.db.initDatabase
+import info.skyblond.daapu.llm.FlagTool
 import info.skyblond.daapu.llm.PostgresChatHistoryProvider
 import io.github.oshai.kotlinlogging.KotlinLogging
-import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
 
 private val logger = KotlinLogging.logger("Application")
 
@@ -56,22 +62,61 @@ fun main() {
                 appendPrompt {
                     user(message)
                 }
-                val text = StringBuilder()
-                // TODO: The docs from koog is misleading.
-                //       Automatic history update on response ONLY works with non-streaming calls
-                //       So with streaming, we might need to manually construct Message.Assistant
-                requestLLMStreaming().filterIsInstance<StreamFrame.TextDelta>().collect { delta ->
-                    // for now we just print them
-                    print(delta.text)
-                    text.append(delta.text)
+                // The docs from koog is misleading.
+                // Automatic history update on response ONLY works with non-streaming calls.
+                // So with streaming, we might need to manually construct Message.Assistant
+                val frames = mutableListOf<StreamFrame>()
+                var isReasoning = false
+                var isOutput = false
+                var hasToolCall = false
+                requestLLMStreaming().requireEndFrame().collect { frame ->
+                    frames.add(frame)
+                    when (frame) {
+                        is StreamFrame.ReasoningDelta -> {
+                            if (!isReasoning) {
+                                isReasoning = true
+                                println("========== Reasoning START ==========")
+                            }
+                            print(frame.text)
+                        }
+
+                        is StreamFrame.ReasoningComplete -> {
+                            isReasoning = false
+                            println("\n========== Reasoning END ==========")
+                        }
+
+                        is StreamFrame.TextDelta -> {
+                            if (!isOutput) {
+                                isOutput = true
+                                println("========== Output START ==========")
+                            }
+                            print(frame.text)
+                        }
+
+                        is StreamFrame.TextComplete -> {
+                            isOutput = false
+                            println("\n========== Output END ==========")
+                        }
+
+                        is StreamFrame.ToolCallDelta -> {
+                            if (!hasToolCall) {
+                                hasToolCall = true
+                                println("========== Tool call START ==========")
+                            }
+                        }
+
+                        is StreamFrame.ToolCallComplete -> {
+                            println("Tool name: " + frame.name)
+                            println("Args: " + frame.content)
+                            println("========== Tool call END ==========")
+                        }
+
+                        is StreamFrame.End -> {
+                            println("Streaming end")
+                        }
+                    }
                 }
-                requestLLMBlocking()
-                // print a new line after finished streaming
-                println()
-                val assistant = Message.Assistant(
-                    parts = listOf(MessagePart.Text(text.toString())),
-                    metaInfo = ResponseMetaInfo.Empty,
-                )
+                val assistant = frames.toMessageResponse()
                 appendPrompt {
                     message(assistant)
                 }
@@ -92,9 +137,25 @@ fun main() {
                 )
             )
         ),
-        llmModel = model,
+        agentConfig = AIAgentConfig(
+            prompt = prompt(
+                id = "chat",
+                params = LLMParams(
+                    additionalProperties = mapOf(
+                        "reasoning_effort" to JsonPrimitive("high")
+                    )
+                )
+            ) {
+                system("You're Gemma 4, LLM trained by Google LLC.")
+            },
+            model = model,
+            maxAgentIterations = 50,
+            responseProcessor = null
+        ),
         strategy = strategy,
-        systemPrompt = "You're Gemma 4.",
+        toolRegistry = ToolRegistry {
+            tool(FlagTool())
+        }
     ) {
         install(ChatMemory) {
             chatHistoryProvider = historyProvider
@@ -103,10 +164,14 @@ fun main() {
     }
 
     // Run the agent
-    runBlocking {
+    val resp = runBlocking {
         agent.run(
-            "What can you do?",
+            "Hello! Who are you?",
             sessionId = "2268100b-6544-4654-9d7a-87f55580fc51"
         )
     }
+    logger.info { "Finish reason ${resp.finishReason}" }
+    logger.info { "Input token ${resp.metaInfo.inputTokensCount}" }
+    logger.info { "Output token ${resp.metaInfo.outputTokensCount}" }
+    logger.info { "Total token ${resp.metaInfo.totalTokensCount}" }
 }
